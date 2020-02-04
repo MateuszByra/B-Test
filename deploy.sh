f3f2ac3e35ba616e3261c9d2cc072f8745716c1c
mat.byra@gmail.com <mat.byra@gmail.com>
2020-02-04 18:33:05 +0100

    test

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ curl -n -X PATCH https://api.heroku.com/apps/${HEROKU_APP_NAME}/formation \
   "updates": [
     {
       "type": "web",
-      "docker_image": "$WEB_DOCKER_IMAGE_ID"
+      "docker_image": "$(docker inspect registry.heroku.com/${HEROKU_APP_NAME}/web --format{{.Id}})"
     }
   ]
 }' \
