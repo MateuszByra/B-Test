7d7cb0590b3d953b1cf77ee10d185f5f332560f4
mat.byra@gmail.com <mat.byra@gmail.com>
2020-02-04 21:23:41 +0100

    WebHost UseKestrel().UseUrls(...) with PORT as env variable

--- Photogallery.Web/Program.cs
+++ Photogallery.Web/Program.cs
@@ -19,6 +19,8 @@ namespace Photogallery.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseUrls("http://0.0.0.0:" + Environment.GetEnvironmentVariable("PORT"))
                 .UseStartup<Startup>();
     }
 }
